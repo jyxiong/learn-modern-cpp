bb6d5be0af8d40652aceb8a9c3152a09e8b44fdf
jyxiong <jingyu.xiong@foxmail.com>
2022-01-14 21:25:27 +0800

    完成 class

--- 2.usability/2.6.0.class.cpp
+++ 2.usability/2.6.0.class.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 class Complex {
 public:
@@ -28,11 +29,12 @@ private:
 
 Complex::Complex(double re, double im): m_re(re), m_im(im)
 {
-
+    std::cout << "Complex default constructor!" << std::endl;
 }
 
 Complex::Complex(int re, int im)
 {
+    std::cout << "Complex common constructor!" << std::endl;
     m_re = static_cast<double>(re);
     m_im = static_cast<double>(im);
 }
@@ -47,13 +49,14 @@ class String {
 public:
     // default constructor
     // String(); // 无参数构造函数 = 默认构造函数
-    String(const char* data=nullptr); //
+    String(const char* data = nullptr); //
 
     // copy constructor
     // 对于有指针的class, 必须要实现拷贝构造函数
     String(const String& str);
 
     // destructor
+    // 对于有指针的class, 必须要实现析构函数
     ~String();
 
     // copt assignment
@@ -62,6 +65,7 @@ public:
 
 public:
     void print_self();
+    char* get_data();
 
 private:
     char* m_data;
@@ -69,31 +73,61 @@ private:
 
 String::String(const char* data)
 {
-
+    std::cout << "String default constructor!" << std::endl;
+    if (data)
+    {
+        m_data = new char[strlen(data) + 1];
+        strcpy(m_data, data);
+    }
+    else
+    {
+        m_data = new char[1];
+        m_data[0] = '\0';
+    }
 }
 
 String::String(const String& str)
 {
-
+    std::cout << "String copy constructor!" << std::endl;
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
 }
 
 String::~String()
 {
-
+    std::cout << "String destructor!" << std::endl;
+    delete[] m_data;
 }
 
 String& String::operator=(const String& str)
 {
+    // 判断是否是同一个对象
+    // 1.效率
+    // 2.防止删除*this后形成野指针，再调用时内存错误
+    if (this == &str)
+        return *this;
+
+    delete[] m_data;
+
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+
     return *this;
 }
 
 void String::print_self()
 {
+    std::cout << m_data << std::endl;
+}
 
+char* String::get_data()
+{
+    return m_data;
 }
 
 int main()
 {
+    // *********************
     Complex complex1(1.1, 1.1);
     Complex complex2(2, 2);
     Complex complex3(complex1);
@@ -103,6 +137,13 @@ int main()
     complex2.print_self();
     complex3.print_self();
     complex4.print_self();
+
+    // *********************
+    String str1("abc");
+    String str2(str1);
+
+    str1.print_self();
+    std::cout << str2.get_data() << std::endl;
 
     return 0;
 }
